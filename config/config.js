3412fdaaa2c8aa7635a9096f29f09fdf762b4e8f
Matildad De Souza <mtld.desouza@gmail.com>
2022-02-24 12:00:08 +0100

    call the News API:sparkles:

---
+++ config/config.js
@@ -0,0 +1,3 @@
+export const API_KEY = `1f4d258abd1e4beb9bd7b816f72990e5`;
+export const endpoint = `https://newsapi.org/v2/top-headlines`;
+export const country = 'fr'
--- screens/All.js
+++ screens/All.js
@@ -1,5 +1,67 @@
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { Text, View, StyleSheet } from 'react-native';
+import moment from 'moment';
+import { NativeBaseProvider, FlatList, ScrollView, Divider, Image, Spinner } from 'native-base';
+import { services } from '../services/services';
+
 
 export default function AllScreen(){
-    return <Text> AllScreen</Text>
+
+    const [newsData, setNewsData] = useState([])
+    useEffect(() => {
+        services('general')
+            .then(data => {
+                setNewsData(data)
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }, [])
+
+    return(
+        <NativeBaseProvider>
+            <ScrollView height={ 850 }>
+                <FlatList
+                    data={newsData}
+                    renderItem={({ item }) => (
+                        <View>
+                           <View style={styles.newsContainer}>
+                                <Text style={styles.title}>
+                                    {item.title}
+                                </Text>
+                                <Text style={styles.date}>
+                                {moment(item.publishedAt).format('LLL')}
+                                </Text>
+                                <Text style={styles.newsDescription}>
+                                    {item.description}
+                                </Text>
+                            </View>
+                            <Divider my={2} bg="#e0e0e0" />
+                        </View> 
+                    )}
+                    keyExtractor={(item) => item.id}
+                />
+            </ScrollView>
+        </NativeBaseProvider>
+    )
 }
+
+const styles = StyleSheet.create({
+    newsContainer: {
+        padding: 10
+    },
+    title: {
+        fontSize: 18,
+        marginTop: 10,
+        fontWeight: "600",
+        color: 'red',
+    },
+    newsDescription: {
+        fontSize: 16,
+        marginTop: 10
+    },
+    date: {
+        fontSize: 14,
+        fontWeight: 'bold',
+    },
+});
